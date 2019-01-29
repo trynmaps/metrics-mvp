fc45828617ff82b3ff8c63210b6d8f2561dbc8fe
Neel Mehta <hathix@users.noreply.github.com>
2019-01-28 19:03:13 -0800

    Create mock-metrics.py

---
+++ mock-metrics.py
@@ -0,0 +1,3 @@
+def get_waiting_time(stop_id, route_id, direction, time_start, time_stop, dates_list);
+  # Returns a list of waiting times in the past so you can do average/range/median/etc
+  return []
