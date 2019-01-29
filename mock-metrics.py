026ab87724d95be976b742569e29681729b3a041
frhino <frhino@users.noreply.github.com>
2019-01-29 08:41:31 -0600

    added comments

--- mock-metrics.py
+++ mock-metrics.py
@@ -1,3 +1,17 @@
-def get_waiting_time(stop_id, route_id, direction, time_start, time_stop, dates_list);
+def get_generic_waiting_time(stop_id, route_id, direction, time_bucket, dates_list);
   # Returns a list of waiting times in the past so you can do average/range/median/etc
+  # For 
+  #  - last weekday
+  #  - last weekend day
+  #  - last full work week
+  #  - last full weekend
+  #  - this month
+  #  - last month
+  #  - last 3 months
+  #  - last year
+  #  - last 5 years
+  #  calculate average for each time_buckets
+  #  calculate variability of each time buckets
+  #  include comparison of actual to scheduled 
+    
   return []
