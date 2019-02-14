381dd9f6a87df71b17cbd40d0b1f16ae4013f1cd
merle <matt@raddcreative.com>
2019-02-13 19:03:32 -0800

    remove jquery and add react-bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BarChart } from 'react-d3-components';
+import Alert from 'react-bootstrap/Alert';
+
 import './App.css';
 import ControlPanel from './components/ControlPanel';
 
@@ -38,6 +40,19 @@ class App extends Component {
         }
       >
         <ControlPanel />
+        <Alert variant="success">
+          <Alert.Heading>Hey, nice to see you</Alert.Heading>
+          <p>
+            Aww yeah, you successfully read this important alert message. This example
+            text is going to run a bit longer so that you can see how spacing within an
+            alert works with this kind of content.
+          </p>
+          <hr />
+          <p className="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things nice
+            and tidy.
+          </p>
+        </Alert>
         <BarChart
           data={data}
           width={400}
