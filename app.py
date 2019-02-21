990a4f33dc218fb348571dacf10241e582c77bac
Neel Mehta <neelmehta96@gmail.com>
2019-02-21 03:12:38 +0000

    gitpod

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ This is the app's main file!
 # configuration
 DEBUG = True
 
+
 # instantiate the app
 app = Flask(__name__, template_folder="./frontend/public")
 app.config.from_object(__name__)
+
 
 # enable CORS
 CORS(app)
@@ -34,12 +36,16 @@ def index():
         date_range=["2019-01-01", "2019-01-02", "2019-01-03"],
         # use the last month; calculate it and turn it into timestamps
         # date_range=[d.date().strftime("%Y-%m-%d") for d in
-            # pd.date_range(pd.datetime.today(), periods=30).tolist()]
+        # pd.date_range(pd.datetime.today(), periods=30).tolist()]
         time_range=("09:00", "17:00")))
+
 
 @app.route('/react', methods=['GET'])
 def react():
     return render_template("index.html")
 
+
 if __name__ == '__main__':
-    app.run()
+    # using 0.0.0.0 makes it externally visible
+    # so gitpod.io can run it
+    app.run(host='0.0.0.0')
