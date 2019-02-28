64275873bb0c87ae59ca12afea14f2677cd401e4
chris <chris@chriss-MacBook-Air.local>
2019-02-27 23:42:29 -0800

    ajax ready

--- metrics-api.py
+++ metrics-api.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
 
 app = Flask(__name__, static_folder='frontend/build')
 
@@ -12,6 +12,10 @@ def serve(path):
     else:
         return send_from_directory('frontend/build', 'index.html')
 
+# sanity check route
+@app.route('/ajaxCall', methods=['POST'])
+def ping_pong():
+    return jsonify('I just made an ajax call :)')
 
 if __name__ == '__main__':
     app.run(use_reloader=True, port=5000, threaded=True)
