8c2e79464274456737ace7a2ee05dd13c9074288
Neel Mehta <neelmehta96@gmail.com>
2019-03-03 20:37:10 +0000

    start merging react and flask apps

--- metrics-api.py
+++ metrics-api.py
@@ -1,7 +1,18 @@
 import os
-from models import metrics
 from flask import Flask, send_from_directory, jsonify, request
+from flask_cors import CORS
 
+from models import metrics
+
+"""
+This is the app's main file!
+"""
+
+# configuration
+DEBUG = True
+
+
+# Create the app
 app = Flask(__name__, static_folder='frontend/build')
 
 # Serve React App
@@ -13,40 +24,43 @@ def serve(path):
     else:
         return send_from_directory('frontend/build', 'index.html')
 
-# hello world
+
+# "Command Line"-esque endpoints
 @app.route('/metrics', methods=['GET'])
-def index():
+def metrics_page():
     route_id = request.args.get('route_id')
     if route_id is None:
-        route_id = '37'
+        route_id = '12'
     stop_id = request.args.get('stop_id')
     if stop_id is None:
         stop_id = '4970'
     date = request.args.get('date')
     if date is None:
         date = "2019-02-01"
-    #date2 = request.args.get('date2')
-    #if date2 is None:
-    #    date2 = "2019-02-02"
-    direction = request.args.get('direction')
-    if direction is None:
-    	direction = "0"
-    #date = [date,date2]
-    print (":) ", route_id, " ", stop_id, " ", date)
+
     return "average waiting time at stop " + stop_id + " for route " + route_id + "  on " + date + " is " + str(metrics.get_average_waiting_time(
         stop_id=stop_id,
         route_id=route_id,
-        direction=direction,
+        direction="O",
         date_range=[date],
         # use the last month; calculate it and turn it into timestamps
         # date_range=[d.date().strftime("%Y-%m-%d") for d in
         # pd.date_range(pd.datetime.today(), periods=30).tolist()]
         time_range=("09:00", "10:00")))
 
-# sanity check route
+
+# ajax test
 @app.route('/ajaxCall', methods=['POST'])
-def ping_pong():
+def ajax():
     return jsonify('I just made an ajax call :)')
 
+
+# sanity check/testing function
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify('hello, world')
+
+
 if __name__ == '__main__':
-    app.run(use_reloader=True, port=5000, threaded=True)
+    app.run(use_reloader=True, port=5000, threaded=True, host='0.0.0.0')
+    # TODO: figure out why host='0.0.0.0' doesn't work
