b88a2207df64f031aa6cc9f544dbbc19deef462f
chris <chris@chriss-MacBook-Air.local>
2019-03-03 11:06:42 -0800

    puching ruote 14

--- frontend/src/actions/action.js
+++ frontend/src/actions/action.js
@@ -2,15 +2,16 @@ import axios from "axios";
 
 export function fetchGraphData(data) {
     return function(dispatch) {
-        axios.get("/metrics", {
+        axios.get("/metricsAPI", {
+        
                 params: {
                     route_id: data[0],
                     stop_id: data[1],
-                    direction: data[2] === "inbound" ? 0 : 1,
+                    direction: data[2] === "inbound" ? "O" : "I",
                     date: data[3],
                     date2: data[4]
                 }
-            })
+            }) 
             .then((response) => {
                 dispatch({ type: "RECEIVED_GRAPH_DATA", payload: response.data })
             })
