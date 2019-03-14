9d8272ff73895d9a677857abb35a7dc4be1478ba
Neel Mehta <neelmehta96@gmail.com>
2019-03-14 02:46:31 +0000

    better comments

--- metrics-api.py
+++ metrics-api.py
@@ -61,6 +61,6 @@ def send_frontend(path):
 
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to something.
+    # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(use_reloader=True, threaded=True, host='0.0.0.0', port=port)
