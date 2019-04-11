8e77d74622e99c96cdac043c78e1a5798fc18793
Sravanti Tekumalla <sravanti150@gmail.com>
2019-04-11 15:16:39 -0700

    Cleanup frontend (#64)

--- frontend/src/components/Info.jsx
+++ frontend/src/components/Info.jsx
@@ -1,36 +1,6 @@
 import React, { Component } from 'react';
 import { css } from 'emotion';
-import { BarChart /*, LineChart */ } from 'react-d3-components';
-
-/*
-const lineData = [
-  {
-    label: '',
-    values: [
-      {
-        x: '5am',
-        y: 2,
-      },
-      {
-        x: '7am',
-        y: 4,
-      },
-      {
-        x: '9am',
-        y: 7,
-      },
-      {
-        x: '11am',
-        y: 7,
-      },
-      {
-        x: '1pm',
-        y: 5,
-      },
-    ],
-  },
-];
-*/
+import { BarChart } from 'react-d3-components';
 
 class Info extends Component {
   constructor(props) {
@@ -39,7 +9,6 @@ class Info extends Component {
   }
 
   render() {
-
     const { graphData, graphError } = this.props;
 
     const histogram = graphData ? graphData.headway_min.histogram : null;
@@ -48,38 +17,36 @@ class Info extends Component {
       <div
         className={css`
          grid-column: col3-start ;
-        grid-row: row1-start / row2-end;
+         grid-row: row2-start / row2-end;
         `
         }
       >
-      {histogram ?
-        /* <LineChart
-          data={lineData}
-          width={400}
-          height={400}
-          margin={
+        {histogram
+          ? (
+            <BarChart
+              data={[{ values: histogram.map(bin => ({ x: `${bin.value}`, y: bin.count })) }]}
+              width={Math.max(400, histogram.length * 70)}
+              className={`css
+                color: 'red'
+              `}
+              height={400}
+              margin={
                   {
-                    top: 10,
-                    bottom: 50,
-                    left: 0.5,
-                    right: 20,
-                  }
-                }
-        /> */
-        <BarChart
-          data={[{values: histogram.map(bin => ({x:''+bin.value, y:bin.count}))}]}
-          width={Math.max(400, histogram.length * 50)}
-          height={400}
-          margin={
-                  {
-                    top: 10,
+                    top: 0,
                     bottom: 50,
                     left: 0,
                     right: 20,
                   }
                 }
-      /> : null }
-      <code>{graphError ? graphError : ''}{ graphData ? JSON.stringify(graphData) : null}</code>
+              xAxis={{label: "minutes"}}
+              barPadding={0.3}
+              style={{fill: 'red'}}
+              yAxis={{innerTickSize: 10, label: "number"}}
+            />
+          ) : null }
+        <code>
+          {graphError || ''}
+        </code>
       </div>
     );
   }
