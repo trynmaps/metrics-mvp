c67156668b5561f2d35e53fbfbaea2c73ea44293
fruit13ok <fruit13ok@gmail.com>
2019-04-12 22:41:47 -0700

    chart color, tick formet, axis label

--- frontend/src/components/Info.jsx
+++ frontend/src/components/Info.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import { css } from 'emotion';
 import { BarChart /*, LineChart */ } from 'react-d3-components';
+import * as d3 from "d3";
+import './Info.css';
 
 /*
-const lineData = [
+to do
+show label for x,y axises
+y change to % of total buses show 
+x frequency
+*/
+
+/* const lineData = [
   {
     label: '',
     values: [
@@ -29,9 +37,9 @@ const lineData = [
       },
     ],
   },
-];
-*/
+]; */
 
+const colorScale = d3.scale.ordinal().range(['red', 'blue', 'green']);
 class Info extends Component {
   constructor(props) {
     super(props);
@@ -41,15 +49,22 @@ class Info extends Component {
   render() {
 
     const { graphData, graphError } = this.props;
-
     const histogram = graphData ? graphData.headway_min.histogram : null;
-
+    // console.log('test',Array.isArray(histogram));
+    // I am trying to wait until histogram's data loaded before access them
+    let totalBus;
+    if(histogram){
+      totalBus = histogram.reduce((totalCount, eachCount)=>{
+        return totalCount+=eachCount.count;
+      },0);
+      console.log('test',totalBus);
+    }
     return (
       <div
         className={css`
          grid-column: col3-start ;
-        grid-row: row1-start / row2-end;
-        `
+          grid-row: row1-start / row2-end;
+          `
         }
       >
       {histogram ?
@@ -66,19 +81,35 @@ class Info extends Component {
                   }
                 }
         /> */
-        <BarChart
-          data={[{values: histogram.map(bin => ({x:''+bin.value, y:bin.count}))}]}
-          width={Math.max(400, histogram.length * 50)}
-          height={400}
-          margin={
-                  {
-                    top: 10,
-                    bottom: 50,
-                    left: 0,
-                    right: 20,
-                  }
-                }
-      /> : null }
+        <div>
+          <div className='wrapper'>
+            <div className='xAxisLabel' >css-x-label</div>
+            <div className='yAxisLabel' >css-y-label</div>
+            <BarChart className='chart1'
+              xAxis={{label: "build-in-x-label"}}
+              yAxis={{label: "build-in-y-label", tickFormat: d3.format("%")}}
+              colorScale={colorScale}
+              data={[
+                // {label: 'a', values: histogram.map(bin => ({x:''+bin.value, y:bin.count}))},
+                // {label: 'b', values: histogram.map(bin => ({x:''+bin.value, y:bin.count}))},
+                {label: 'c', values: histogram.map(bin => ({
+                  x:''+bin.value, 
+                  y:(bin.count/totalBus)
+                }))}
+              ]}
+              width={Math.max(400, histogram.length * 70)}
+              height={400}
+              margin={{top: 10, bottom: 30, left: 50, right: 10}}/>
+          </div>
+          
+          <BarChart
+            colorScale={colorScale}
+            data={[{label: 'a', values: histogram.map(bin => ({x:''+bin.value, y:bin.count}))}]}
+            width={Math.max(400, histogram.length * 70)}
+            height={400}
+            margin={{top: 10, bottom: 50, left: 50, right: 20,}}/>
+        </div> : 
+        null }
       <code>{graphError ? graphError : ''}{ graphData ? JSON.stringify(graphData) : null}</code>
       </div>
     );
