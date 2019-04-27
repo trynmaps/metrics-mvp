f2fda54149d6f66d41089f40363cef4c2baca7fe
Jim Lee <jimleeisme@gmail.com>
2019-04-26 18:00:50 -0700

    Resolve requested changes. Add redux-first-router-link.

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component, Fragment } from 'react';
+import Link from 'redux-first-router-link';
 
 class About extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
-      <h3>About Us</h3>
+      <Fragment>
+        <h3>About Us</h3>
+        <Link to={{ type: 'HOME' }}>Home</Link>
+      </Fragment>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return state;
-};
-
-export default connect(mapStateToProps)(About);
+export default About;
