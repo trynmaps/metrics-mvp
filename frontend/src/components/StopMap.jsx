0e79e6c07f0694ffb6278ad0e941f7394a9e4226
exxonvaldez <exxonvaldez@yahoo.com>
2019-05-01 13:59:07 -0700

    Checkpoint of map-based stop finder/selector.

--- frontend/src/components/StopMap.jsx
+++ frontend/src/components/StopMap.jsx
@@ -1,23 +1,17 @@
-import React, { Component, createRef, Fragment, Link } from 'react';
+import React, { Component, createRef, Fragment } from 'react';
 import { css } from 'emotion';
-import DatePicker from 'react-date-picker';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import { latLngBounds } from 'leaflet';
 
 import DropdownControl from './DropdownControl';
 
 import { Map, TileLayer, Marker, Popup, CircleMarker, Tooltip } from 'react-leaflet'
 
-// todo: figure out why sometimes the zoom gets reset (probably a race condition with setState, need callback)
-// todo: make start stops clickable (equivalent to the buttons)
-// todo: think about start stops legibility, different color or shape for inbound vs outbound?
-// todo: nice if we can intelligently handle multiple routes at the same stop
-// todo: when pickers are used to change from stop, need to update map
+class StopMap extends Component {
 
-class ControlPanel extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -29,60 +23,31 @@ class ControlPanel extends Component {
       date: new Date('2019-04-08T03:50'),
       startTimeStr: null,
       endTimeStr: null,
-
-
-
-
-
       
       hasLocation: false, // not geolocated
       latLng: {
         lat: 37.7793, // city hall
         lng: -122.4193,
       },
-      startMarkers: [],
-      downstreamArray: [],
-      routeMarkers: [],
+      stopMarkers: [],
     }
     
     this.mapRef = createRef();
-
-
-
+    
+    
     
   }
 
+
+          
   componentDidUpdate() {
     const selectedRoute = this.getSelectedRouteInfo();
     if (selectedRoute) {
-      //if (!selectedRoute.directions) {
-      
-        // hack: if any route config is missing, get next config
-        
-        const { routes } = this.props;
-        if (routes) {
-          let i = 0;
-          for (i = 0; i < routes.length; i++) { // optimize this on back end
-            const route = routes[i];
-            if (!route.directions) {
-              this.props.fetchRouteConfig(route.id);
-              break;
-            }
-          }
-          
-          if (i === routes.length) { // all routes loaded
-            if (!this.state.directionId && selectedRoute.directions.length > 0) {
-              this.setState({ directionId: selectedRoute.directions[0].id });
-            }
-          }
-        }
-      
-        //original: this.props.fetchRouteConfig(this.state.routeId);
-      //} else if (!this.state.directionId && selectedRoute.directions.length > 0) {
-      //  this.setState({ directionId: selectedRoute.directions[0].id });
-      //}
-      
-      
+      if (!selectedRoute.directions) {
+        this.props.fetchRouteConfig(this.state.routeId);
+      } else if (!this.state.directionId && selectedRoute.directions.length > 0) {
+        this.setState({ directionId: selectedRoute.directions[0].id });
+      }
     }
   }
 
@@ -112,9 +77,7 @@ class ControlPanel extends Component {
     this.updateGraphData();
   }
 
-  setDate = date => this.setState({ date }, this.updateGraphData)
-
-  setTimeRange = timeRange => {
+    setTimeRange = timeRange => {
     if (!timeRange) {
       this.setState({ startTimeStr: null, endTimeStr: null }, this.updateGraphData);
     } else {
@@ -133,25 +96,12 @@ class ControlPanel extends Component {
   }
 
   onSelectFirstStop = (stopId) => {
-    const { directionId, secondStopId } = this.state;
+    const { directionId } = this.state;
     const selectedRoute = { ...this.getSelectedRouteInfo() };
     const secondStopInfo = this.getStopsInfoInGivenDirection(selectedRoute, directionId);
     const secondStopListIndex = secondStopInfo.stops.indexOf(stopId);
     const secondStopList = secondStopInfo.stops.slice(secondStopListIndex + 1);
-    
-    let newSecondStopId = secondStopId;
-
-    // If the "to stop" is not set or is not valid for the current "from stop",
-    // set a default "to stop" that is some number of stops down.  If there aren't
-    // enough stops, use the end of the line.
-
-    const nStops = 5;
-
-    if (secondStopId == null || !secondStopList.includes(secondStopId)) {
-        newSecondStopId = secondStopList.length >= nStops ? secondStopList[nStops-1] :
-            secondStopList[secondStopList.length-1];
-    }
-    this.setState({ firstStopId: stopId, secondStopId: newSecondStopId, secondStopList }, this.selectedStopChanged);
+    this.setState({ firstStopId: stopId, secondStopList }, this.selectedStopChanged);
   }
 
   onSelectSecondStop = (stopId) => {
@@ -188,7 +138,6 @@ class ControlPanel extends Component {
   }
 
   selectedStopChanged = () => {
-    this.plotSelectedRoute();
     this.updateGraphData();
   }
 
@@ -202,20 +151,12 @@ class ControlPanel extends Component {
     return routes ? routes.find(route => route.id === routeId) : null;
   }
 
-  handleGeoLocate = (e) => {
-    e.preventDefault();
-    const map = this.mapRef.current;
-    if (map != null) {
-      map.leafletElement.locate(); // this is for geolocation, see https://leafletjs.com/examples/mobile/
-    }
-   }
-      
-
-
    handleClick = (e) => {
     const map = this.mapRef.current
     if (map != null) {
       this.handleLocationFound(e);
+      //map.leafletElement.locate() // this is for geolocation, see https://leafletjs.com/examples/mobile/, should also
+      // support location at click
     }
    }
    
@@ -223,33 +164,13 @@ class ControlPanel extends Component {
     this.setState({
       hasLocation: true,
       latLng: e.latlng,
-    }, this.findAndPlotStops);
-    
+    })
+    this.findAndPlotStops();
   }   
   
   findAndPlotStops() {
      const stops = this.findStops();
-     
-     // what if we also plot all the downstream stops
-     
-     let downstreamArray = [];
-     for (let stop of stops) {
-         const selectedRoute = this.props.routes.find(route => route.id === stop.routeID);
-         
-         const secondStopInfo = stop.direction;//this.getStopsInfoInGivenDirection(selectedRoute, stop.direction);
-         const secondStopListIndex = secondStopInfo.stops.indexOf(stop.stopID);
-         const secondStopList = secondStopInfo.stops.slice(secondStopListIndex + 1);
-
-        
-         const downstreamStops = secondStopList.map(stopID => selectedRoute.stops[stopID]);
-         downstreamArray.push(downstreamStops);
-     }
-    
-    // to do: indicators for firstStop and secondStop 
-     
-     this.setState({ startMarkers: stops,
-                     downstreamArray: downstreamArray
-      });
+     this.setState({ stopMarkers: stops });
   }
   
   // ugly brute force method:  for each route, fetch route config. for each direction of each route
@@ -264,6 +185,7 @@ class ControlPanel extends Component {
     
     for (let i = 0; i < routes.length; i++) { // optimize this on back end
       const route = routes[i];
+      //const route = this.getSelectedRouteInfo();
       if (route.directions) {
         for (let direction of route.directions) {
           const stopList = direction.stops;
@@ -274,6 +196,8 @@ class ControlPanel extends Component {
           nearest.direction = direction;
           stopsByRouteAndDir.push(nearest);
         }
+      } else { // no good, need a way to fetch all route configs
+        this.props.fetchRouteConfig(route.id);
       }
     }
     // sort by distance and truncate by distance
@@ -319,149 +243,58 @@ class ControlPanel extends Component {
     return degLength * Math.sqrt(deltaLat * deltaLat + deltaLon * deltaLon);
   }
 
-
-  plotSelectedRoute() {
-    
-    const selectedRoute = this.getSelectedRouteInfo();
-    const { directionId, firstStopId, secondStopId } = this.state;
-    const secondStopInfo = this.getStopsInfoInGivenDirection(selectedRoute, directionId);
-    const secondStopListIndex = secondStopInfo.stops.indexOf(firstStopId);
-    const secondStopList = secondStopInfo.stops.slice(secondStopListIndex + 1);
-    
-    
-    const stops = secondStopList.map(stopID => selectedRoute.stops[stopID]);
-     
-    
-    // to do: indicators for firstStop and secondStop 
-     
-    this.setState({ routeMarkers: stops });
-  }
-
-
-
-
-
-
-
-
-
-
-
-  // toggleTimekeeper(val) {
-  //   // this.setState({ displayTimepicker: val });
-  // }
-
   render() {
     const { routes } = this.props;
     
     
-      
-        
     const {
       date, routeId, directionId, firstStopId, secondStopId, secondStopList, startTimeStr, endTimeStr
     } = this.state;
 
-    const timeRange = (startTimeStr || endTimeStr) ? (startTimeStr + '-' + endTimeStr) : '';
-
     const selectedRoute = this.getSelectedRouteInfo();
     const selectedDirection = (selectedRoute && selectedRoute.directions && directionId)
       ? selectedRoute.directions.find(dir => dir.id === directionId) : null;
-
-
-
-
-
-
-
-
-
+      
+      
     const marker = this.state.hasLocation ? (
       <Marker position={this.state.latLng} opacity="0.01">
         <Popup>You are here</Popup>
       </Marker>
 ) : null;      
 
-    const StartMarkers = () => {  
+    const StopMarkers = () => {  
 
-      const items = this.state.startMarkers.map(startMarker => {
-        const position = [ startMarker.stop.lat, startMarker.stop.lon ];
-        return <CircleMarker key={startMarker.stopID} center={position} opacity="0.1">
+      const items = this.state.stopMarkers.map(stopMarker => {
+        const position = [ stopMarker.stop.lat, stopMarker.stop.lon ];
+        return <CircleMarker key={stopMarker.stopID} center={position} opacity="0.1">
           <Tooltip permanent="true">
-          {startMarker.routeID}
+          {stopMarker.routeID}
           </Tooltip>
-          <Popup>{startMarker.stop.title}<br/>
-            {startMarker.routeTitle}<br/>
-            {startMarker.direction.title}<br/>
-            {Math.round(startMarker.miles * 5280)} feet
+          <Popup>{stopMarker.stop.title}<br/>
+            {stopMarker.routeTitle}<br/>
+            {stopMarker.direction.title}<br/>
+            {Math.round(stopMarker.miles * 5280)} feet
           </Popup>
         </CircleMarker>
       });
       return <Fragment>{items}</Fragment>
     }
-    
-    
-    const DownstreamMarkers = () => {  
-
-      const items = this.state.downstreamArray.map(routeMarkers =>
-        routeMarkers.map(routeMarker => {
-           const position = [ routeMarker.lat, routeMarker.lon ]; // just stops directly, was routeMarker.stop.lat
-           return <CircleMarker key={routeMarker.stopID} center={position} opacity="0.1">
-             </CircleMarker>
-           }
-        )
-      );
-      return <Fragment>{items}</Fragment>
-    }
-    
-
-    const RouteMarkers = () => {  
-
-      const items = this.state.routeMarkers.map(routeMarker => {
-        const position = [ routeMarker.lat, routeMarker.lon ]; // just stops directly, was routeMarker.stop.lat
-        return <CircleMarker key={routeMarker.stopID} center={position} opacity="0.1">
-        </CircleMarker>
-      });
-      return <Fragment>{items}</Fragment>
-    }
-
-
-
  
  
     const FromButtons = () => {  
 
-      const items = this.state.startMarkers.map(startMarker => {
-        return <Button variant="secondary" key={startMarker.stopID}
-          onClick={e => { // todo: after this button is clicked, plot only the stops for this route/dir
-          
-           this.setState({routeId: startMarker.routeID,
-             directionId: startMarker.direction.id});
-             this.onSelectFirstStop(startMarker.stopID);
-             }
-           }>
-          {startMarker.routeTitle} - {startMarker.direction.title}
+      const items = this.state.stopMarkers.map(stopMarker => {
+        return <Button variant="secondary" key={stopMarker.stopID}>
+          {stopMarker.routeTitle} {stopMarker.direction.title}
           </Button>
       });
       return <Fragment>{items}</Fragment>
     }
  
  
-    const bounds = this.state.startMarkers.length > 1 ?
-      latLngBounds(this.state.startMarkers.map(startMarker => [startMarker.stop.lat, startMarker.stop.lon])).pad(0.25) : null;
+    const bounds = this.state.stopMarkers.length > 1 ?
+      latLngBounds(this.state.stopMarkers.map(stopMarker => [stopMarker.stop.lat, stopMarker.stop.lon])).pad(0.25) : null;
  
-
-
-
-
-
-
-
-
-
-
-
-
-
     return (
       <div className={css`
           color: #fff;
@@ -469,43 +302,46 @@ class ControlPanel extends Component {
           padding: 10px;
           margin-right: 20px;
           grid-column: col1-start / col3-start;
-          grid-row: row2-start ;
+          grid-row: row3-start ;
           font-family: 'Oswald', sans-serif;
       `
       }
       >
         <Card bg="light" style={{ color: 'black' }}>
-          <Card.Header>Visualize Route</Card.Header>
-          <DatePicker
-            value={date}
-            onChange={this.setDate}
-            className={css`
-           padding: 10px!important;
-           display: block;
-           width: 100%
-         `}
-          />
-        <ListGroup.Item>
-          <DropdownControl
-            title="Time Range"
-            name="time_range"
-            variant="info"
-            value={timeRange}
-            onSelect={this.setTimeRange}
-            options={
-                [
-                    {label:'All Day', key:''},
-                    {label:'Daytime (7AM - 7PM)', key:'07:00-19:00'},
-                    {label:'Early Morning (3AM - 7AM)', key:'03:00-07:00'},
-                    {label:'AM Peak (7AM - 10AM)', key:'07:00-10:00'},
-                    {label:'Midday (10AM - 4PM)', key:'10:00-16:00'},
-                    {label:'PM Peak (4PM - 7PM)', key:'16:00-19:00'},
-                    {label:'Late Evening (7PM - 3AM)', key:'19:00-03:00+1'},
-                ]
-        }
-          />
-        </ListGroup.Item>
+          <Card.Header>Map</Card.Header>
+          
+      <Map
+        style={{
+          height:"20vw"
+        }}
+        bounds={bounds}
+        center={this.state.latLng}
+        length={4}
+        onClick={this.handleClick}
+        onLocationfound={this.handleLocationFound}
+        ref={this.mapRef}
+        zoom={ bounds ? null : 13}
+        minZoom={11}
+        maxZoom={18}
+        >
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          //url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png"
+          opacity={0.5}
+        /> {/* see http://maps.stamen.com for details */}
+        {/*marker*/}
+        <StopMarkers/>
+        
+</Map>          
+          
           <ListGroup variant="flush">
+          <div className={css`max-height:300px; overflow:scroll;`}>
+            <ListGroup.Item>
+              <FromButtons/>
+            </ListGroup.Item>
+            </div>
+            
             <ListGroup.Item>
               <DropdownControl
                 title="Route"
@@ -578,55 +414,13 @@ class ControlPanel extends Component {
             }
           </ListGroup>
         </Card>
-
-
-
-
-        <Card bg="light" style={{ color: 'black' }}>
-          <Card.Header>Map <Button className="float-sm-right" onClick={this.handleGeoLocate}>Go to my location</Button></Card.Header>
-          
-      <Map
-        style={{
-          height:"40vw"
-        }}
-        bounds={bounds}
-        center={this.state.latLng}
-        length={4}
-        onClick={this.handleClick}
-        onLocationfound={this.handleLocationFound}
-        ref={this.mapRef}
-        zoom={ bounds ? null : 13}
-        minZoom={11}
-        maxZoom={18}
-        >
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          //url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          url="http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png"
-          opacity={0.5}
-        /> {/* see http://maps.stamen.com for details */}
-        {/*marker*/}
-        <StartMarkers/>
-        <DownstreamMarkers/>
-        <RouteMarkers/>
-        
-</Map>          
-          
-          <ListGroup variant="flush">
-          <div className={css`max-height:300px; overflow:scroll;`}>
-            <ListGroup.Item>
-              <FromButtons/>
-            </ListGroup.Item>
-          </div>
-          </ListGroup>
-        </Card>
       </div>
     );
   }
 }
 
-ControlPanel.propTypes = {
-  fetchGraphData: PropTypes.func.isRequired,
-};
+//StopMap.propTypes = {
+  //fetchGraphData: PropTypes.func.isRequired,
+//};
 
-export default ControlPanel;
+export default StopMap;
