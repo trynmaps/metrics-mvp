8a9270a7ac542475b0c2ab72a6230226e54259ec
fruit13ok <fruit13ok@gmail.com>
2019-05-08 19:47:31 -0700

    follow Jim's way, add route to Home, About, Landing with redux-first-router-link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,14 @@ import { connect } from 'react-redux';
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
+import Landing from './components/Landing';
 import NotFound from './components/NotFound';
 
 const App = ({ page }) => {
   const components = {
     Home: <Home />,
     About: <About />,
+    Landing: <Landing />,
     NotFound: <NotFound />
   };
   return (
