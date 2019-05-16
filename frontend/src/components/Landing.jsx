dd5df060932f03be67411fbd4341aada5c10e5c8
fruit13ok <fruit13ok@gmail.com>
2019-05-15 19:21:29 -0700

    fixed indentation

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,28 +1,40 @@
 import React, { Fragment } from 'react';
-import { Link, NavLink } from 'redux-first-router-link';
+import { NavLink } from 'redux-first-router-link';
 
 const Landing = () => (
-    <Fragment>
-        <button><NavLink 
+  <Fragment>
+    <button>
+      <NavLink 
         to={{ type: 'HOME' }}
         activeStyle={{ fontWeight: "bold", color: 'purple' }}
         exact={true}
         strict={true}
-        >Home</NavLink></button>
-        <button><NavLink 
+      >
+        Home
+      </NavLink>
+    </button>
+    <button>
+      <NavLink 
         to={{ type: 'ABOUT' }}
         activeStyle={{ fontWeight: "bold", color: 'purple' }}
         exact={true}
         strict={true}
-        >About</NavLink></button>
-        <button><NavLink 
+      >
+        About
+      </NavLink>
+    </button>
+    <button>
+      <NavLink 
         to={{ type: 'LANDING' }}
         activeStyle={{ fontWeight: "bold", color: 'purple' }}
         exact={true}
         strict={true}
-        >Landing</NavLink></button>
-        <h3>Landing Page</h3>
-    </Fragment>
+      >
+        Landing
+      </NavLink>
+    </button>
+    <h3>Landing Page</h3>
+  </Fragment>
 );
 
 export default Landing;
