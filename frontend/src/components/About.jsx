9fbcd688ceb507b534e2035856ead6e33f21c4cf
Arun Gupta <akgupta89@gmail.com>
2019-06-12 21:02:49 -0700

    pre commit hook test

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { NavLink } from 'redux-first-router-link';
 
 export default function About() {
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,3 @@
-
 // When running in dev mode via docker-compose or GitPod, REACT_APP_METRICS_BASE_URL is set
 // to the URL of the metrics API server. When REACT_APP_METRICS_BASE_URL is not set, metrics
 // will be loaded from the same domain as the React app.
