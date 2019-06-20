98230bd89498555d37eb6ddebadb67ef1c68c3e6
akgupta89 <40367955+akgupta89@users.noreply.github.com>
2019-06-19 21:22:21 -0700

    Refactoring fix (#137)
    * patches changes
    
    * fixes refactoring errors
    
    * added todo

--- frontend/src/components/MapStops.jsx
+++ frontend/src/components/MapStops.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { Map, TileLayer, CircleMarker, Popup, Marker } from 'react-leaflet';
+import { Map, TileLayer, CircleMarker } from 'react-leaflet';
 
 const INBOUND_COLOR = 'blue';
 const INBOUND_RADIUS = 4;
@@ -9,7 +9,7 @@ const OUTBOUND_RADIUS = 4;
 const SF_COORDINATES = { lat: 37.7793, lng: -122.419 };
 const ZOOM = 13;
 
-function MapStops(props) {
+function MapStops() {
   //
   // handleRouteSelect = (route) => {
   //
@@ -22,9 +22,9 @@ function MapStops(props) {
     inboundRadius,
     outboundColor,
     outboundRadius,
-  } = props;
+  } = [];
   const mapClass = { width: '500px', height: '500px' };
-  const { routeStops } = this.props;
+  const { routeStops } = [];
 
   return (
     <Map
@@ -33,7 +33,9 @@ function MapStops(props) {
       style={mapClass}
     >
       <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        attribution='&copy; <a href="http://osm.org/copyright">
+          OpenStreetMap
+        </a> contributors'
         url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
       />
       {routeStops
