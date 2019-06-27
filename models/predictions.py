9275eee26f3c22c88cabed585d4b9591882c0f9b
vicky11z <vicky11zhang@gmail.com>
2019-06-26 20:52:10 -0700

    predictions by stop

--- models/predictions.py
+++ models/predictions.py
@@ -1,14 +1,19 @@
 from datetime import datetime, time
+import re
+import os
+import json
+from . import util, siri_api, constants
 
 
 class Prediction:
-    def __init__(self, queried_time: str, route_id: str, stop_id: str, vehicle_id: str, arrival_time: str, direction: str):
+    def __init__(self, queried_time: str, route_id: str, stop_id: str, vehicle_id: str, arrival_time: str, direction: str, agency: str):
         self.queried_time = queried_time
         self.route_id = route_id
         self.stop_id = stop_id
         self.vehicle_id = vehicle_id
         self.arrival_time = arrival_time
         self.direction = direction
+        self.agency = agency
 
     def get_predicted_minutes_to_arrival(self):
         queried_time_stamp = datetime.strptime(
@@ -20,19 +25,77 @@ class Prediction:
         prediction_in_m = divmod(prediction_in_s, 60)[0]
         return prediction_in_m
 
+    @classmethod
+    def from_data(cls, data: dict):
+        return cls(
+            queried_time=data['queried_time'],
+            route_id=data['route_id'],
+            stop_id=data['stop_id'],
+            vehicle_id=data['vehicle_id'],
+            arrival_time=data['arrival_time'],
+            direction=data['direction'],
+            agency=data['agency'],
+        )
 
-class Predictions:
-    def __init__(self, queried_time: str):
-        self.stop_id_to_predictions = {}
+
+class PredictionsByStop:
+    def __init__(self, stop_id: str, queried_time, prediction_data: list, agency: str):
         self.queried_time = queried_time
+        self.stop_id = stop_id
+        self.prediction_data = prediction_data
+        self.agency = agency
 
-    def add_prediction_to_stop(self, prediction: Prediction):
-        stop_id = prediction.stop_id
-        if stop_id not in self.stop_id_to_predictions:
-            self.stop_id_to_predictions[stop_id] = []
-        self.stop_id_to_predictions[stop_id].append(prediction)
-    
-    def get_predictions_for_stop(self, stop_id: str) -> Prediction:
-      if stop_id not in self.stop_id_to_predictions:
-        raise Exception(f"Stop ID not found for predictions at time {self.queried_time}")
-      return self.stop_id_to_predictions['stop_id']
+    def add_prediction(self, prediction: Prediction):
+        self.prediction_data.append(prediction)
+
+    @classmethod
+    def from_data(cls, data: dict):
+        predictions = [Prediction.from_data(p) for p in data['predictions']]
+        return cls(
+            stop_id=data['stop_id'],
+            queried_time=data['queried_time'],
+            prediction_data=predictions,
+            agency=data['agency'],
+        )
+
+    def get_data(self):
+        return {
+            'queried_time': self.queried_time,
+            'stop_id': self.stop_id,
+            'prediction_data': self.prediction_data,
+            'agency': self.agency,
+        }
+
+
+def get_cache_path(agency: str, stop_id: str, datetime: datetime) -> str:
+    datetime_str = str(datetime)
+    if re.match(r'^[\w\-]+$', agency) is None:
+        raise Exception(f"Invalid agency: {agency}")
+
+    if re.match(r'^[\w\-]+$', stop_id) is None:
+        raise Exception(f"Invalid stop id: {stop_id}")
+
+    if re.match(r'^[\w\-]+$', datetime_str) is None:
+        raise Exception(f"Invalid date: {datetime_str}")
+
+    return os.path.join(util.get_data_dir(), f"predictions_{agency}/{datetime_str}/predictions_{agency}_{datetime_str}_{stop_id}.json")
+
+
+def get_predictions_by_stop(agency: str, stop_id: str, datetime: datetime) -> PredictionsByStop:
+    # TODO: round to the nearest 5 minutes
+    cache_path = get_cache_path(agency, stop_id, datetime)
+    try:
+        with open(cache_path, "r") as f:
+            text = f.read()
+            return PredictionsByStop.from_data(json.loads(text))
+    except FileNotFoundError:
+        pass
+
+    # eventually want to call out to S3 here
+
+    predictions_by_stop = siri_api.get_predictions_by_stop_id(
+        constants.SIRI_API_KEY_TEST, 'SF', stop_id)
+    with open(cache_path, "w") as f:
+        f.write(predictions_by_stop.get_data())
+
+    return predictions_by_stop
--- models/siri_api.py
+++ models/siri_api.py
@@ -4,10 +4,8 @@ import json
 from . import predictions as p
 
 
-def call_predictions_api(api_key: str, agency: str, stopCode: str = None) -> p.Predictions:
+def get_all_predictions(api_key: str, agency: str) -> p.Predictions:
     url = f"http://api.511.org/transit/StopMonitoring?api_key={api_key}&agency={agency}"
-    if stopCode:
-        url += f"&stopCode={stopCode}"
     resp = requests.get(url)
     if resp.status_code != 200:
         resp.raise_for_status()
@@ -15,15 +13,56 @@ def call_predictions_api(api_key: str, agency: str, stopCode: str = None) -> p.P
     resp_json = resp.json()
     all_predictions = resp_json['ServiceDelivery']['StopMonitoringDelivery']['MonitoredStopVisit']
     queried_time = resp_json['ServiceDelivery']['ResponseTimestamp']
-    predictions_obj = p.Predictions(queried_time)
+    # predictions_obj = p.Predictions(queried_time, agency)
+    stops_to_predictions = {}
     for prediction in all_predictions:
-        queried_time = prediction['RecordedAtTime']
-        route_id = prediction['MonitoredVehicleJourney']['LineRef']
-        stop_id = prediction['MonitoredVehicleJourney']['MonitoredCall']['StopPointRef']
-        vehicle_id = prediction['MonitoredVehicleJourney']['FramedVehicleJourneyRef']['DatedVehicleJourneyRef']
-        arrival_time = prediction['MonitoredVehicleJourney']['MonitoredCall']['AimedArrivalTime']
-        direction = prediction['MonitoredVehicleJourney']['DirectionRef']
-        new_prediction = p.Prediction(
-            queried_time, route_id, stop_id, vehicle_id, arrival_time, direction)
-        predictions_obj.add_prediction_to_stop(new_prediction)
-    return predictions_obj
+        prediction_data = parse_prediction_return(prediction)
+        prediction_data['agency'] = agency
+        new_prediction = p.Prediction.from_data(prediction_data)
+        stop_id = new_prediction.stop_id
+        if stop_id not in stops_to_predictions:
+            stops_to_predictions[stop_id] = p.PredictionsByStop(
+                stop_id, queried_time, [], agency)
+        stops_to_predictions[stop_id].add_prediction(new_prediction)
+    return stops_to_predictions
+
+
+def get_predictions_by_stop_id(api_key: str, agency: str, stop_id: str) -> p.PredictionsByStop:
+    url = f"http://api.511.org/transit/StopMonitoring?api_key={api_key}&agency={agency}&stopCode={stop_id}"
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        resp.raise_for_status()
+    resp.encoding = 'utf-8-sig'
+    resp_json = resp.json()
+    all_predictions = resp_json['ServiceDelivery']['StopMonitoringDelivery']['MonitoredStopVisit']
+    queried_time = resp_json['ServiceDelivery']['ResponseTimestamp']
+    predictions = []
+    for pred in all_predictions:
+        pred_data = parse_prediction_return(pred)
+        pred_data['agency'] = agency
+        prediction = p.Prediction.from_data(pred_data)
+        predictions.append(prediction)
+    predictions_by_stop = p.PredictionsByStop(
+        stop_id, queried_time, predictions, agency)
+    return predictions_by_stop
+
+
+def parse_prediction_return(prediction: dict) -> dict:
+    return {
+        'queried_time': prediction['RecordedAtTime'],
+        'route_id': prediction['MonitoredVehicleJourney']['LineRef'],
+        'stop_id': prediction['MonitoredVehicleJourney']['MonitoredCall']['StopPointRef'],
+        'vehicle_id': prediction['MonitoredVehicleJourney']['FramedVehicleJourneyRef']['DatedVehicleJourneyRef'],
+        'arrival_time': prediction['MonitoredVehicleJourney']['MonitoredCall']['AimedArrivalTime'],
+        'direction': prediction['MonitoredVehicleJourney']['DirectionRef'],
+    }
+
+
+pbs = get_predictions_by_stop_id(
+    '2b81cac7-b863-4f47-bc87-aab157fc052c', 'SF', '13009')
+print(pbs)
+print(pbs.stop_id)
+print(pbs.prediction_data)
+for p in pbs.prediction_data:
+    print(p.vehicle_id)
+    print(p.get_predicted_minutes_to_arrival())
