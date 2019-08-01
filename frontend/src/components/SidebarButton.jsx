75fda72a38bd0f094aef2e2e2d3040796f62af27
Thierry Delbart <thierry@thde.pro>
2019-08-01 00:59:57 -0300

    Add cursor: pointer for active item

--- frontend/src/components/SidebarButton.jsx
+++ frontend/src/components/SidebarButton.jsx
@@ -23,7 +23,7 @@ class SidebarButton extends React.Component {
     }
 
     render() {
-        const activeStyle = { fontWeight: "bold", color: '#3f51b5', textDecoration: 'none' };
+        const activeStyle = { fontWeight: "bold", color: '#3f51b5', textDecoration: 'none', cursor: 'pointer' };
         return <div>
               <IconButton
               color="inherit"
