20705f9a3515bc46bb2c5ba5432949150ef58923
exxonvaldez <exxonvaldez@gmail.com>
2019-07-31 22:54:02 -0700

    Add QuadrantChart, clean up diagnostic screen to match other screens.

--- frontend/src/screens/DataDiagnostic.jsx
+++ frontend/src/screens/DataDiagnostic.jsx
@@ -1,44 +1,29 @@
 import React, { Fragment } from 'react';
 import Grid from '@material-ui/core/Grid';
-import Drawer from '@material-ui/core/Drawer';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import AppBar from '@material-ui/core/AppBar';
-import { NavLink } from 'redux-first-router-link';
 
 import { connect } from 'react-redux';
 
 import TravelTimeChart from '../components/TravelTimeChart';
+import QuadrantChart from '../components/QuadrantChart';
+import SidebarButton from '../components/SidebarButton';
 
 import {
   fetchRoutes,
   fetchPrecomputedWaitAndTripData,
 } from '../actions';
 
+/**
+ * Data diagnostic screen.  Plots a quadrant chart and travel across stops
+ * for all routes.  Access via /dataDiagnostic.
+ */
 class DataDiagnostic extends React.Component {
   componentDidMount() {
     if (!this.props.routes) {
       this.props.fetchRoutes();
     }
     this.props.fetchPrecomputedWaitAndTripData(this.props.graphParams);   
-  }
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      drawerOpen: false,
-    };
-
-    this.handleToggleDrawer = this.handleToggleDrawer.bind(this);
-  }
-
-  handleToggleDrawer() {
-    this.setState({ drawerOpen: !this.state.drawerOpen });
   }
 
   render() {
@@ -61,49 +46,14 @@ class DataDiagnostic extends React.Component {
       <Fragment>
         <AppBar position="relative">
           <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleToggleDrawer}
-              edge="start"
-            >
-              <MenuIcon />
-            </IconButton>
-        <NavLink
-        to={{ type: 'DASHBOARD' }}
-        activeStyle={{ fontWeight: "bold", color: 'purple' }}
-        exact={true}
-        strict={true}
-      >
-        Muni
-        </NavLink>
-
+            <SidebarButton />        
+            <div>Muni</div>
           </Toolbar>
         </AppBar>
 
-        <Drawer
-          variant="persistent"
-          anchor="left"
-          open={this.state.drawerOpen}
-          style={{ width: 500 }}
-        >
-          <div style={{ width: 250 }}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleToggleDrawer}
-              edge="start"
-            >
-              <ChevronLeftIcon />
-            </IconButton>
-            <List>
-              <ListItem>Test</ListItem>
-              <ListItem>Test</ListItem>
-            </List>
-          </div>
-        </Drawer>
-
         Date: { graphParams.date} Time: { graphParams.start_time } - { graphParams.end_time }
+        
+        <QuadrantChart/>
         <Grid container spacing={0}>
             { charts }
         </Grid>
