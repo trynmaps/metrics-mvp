b795ad6e6aeda5afa5dbd111f810b2f710a5953c
exxonvaldez <exxonvaldez@gmail.com>
2019-08-03 13:37:15 -0700

    Minor updates.
    - DataDiagnostic: Converted to functional component, added DateTimePanel.
    - SideBarButton: Added semi-hidden link to data diagnostic page (last navlink is a dot)

--- frontend/src/screens/DataDiagnostic.jsx
+++ frontend/src/screens/DataDiagnostic.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
@@ -8,58 +8,68 @@ import { connect } from 'react-redux';
 import TravelTimeChart from '../components/TravelTimeChart';
 import QuadrantChart from '../components/QuadrantChart';
 import SidebarButton from '../components/SidebarButton';
+import DateTimePanel from '../components/DateTimePanel';
+import { makeStyles } from '@material-ui/core/styles';
 
 import {
   fetchRoutes,
   fetchPrecomputedWaitAndTripData,
 } from '../actions';
 
+const useStyles = makeStyles({
+  title: {
+    flexGrow: 1,
+  },
+});
+
 /**
  * Data diagnostic screen.  Plots a quadrant chart and travel across stops
  * for all routes.  Access via /dataDiagnostic.
  */
-class DataDiagnostic extends React.Component {
-  componentDidMount() {
-    if (!this.props.routes) {
-      this.props.fetchRoutes();
+function DataDiagnostic(props) {
+  
+  useEffect(() => {
+    if (!props.routes) {
+      props.fetchRoutes();
     }
-    this.props.fetchPrecomputedWaitAndTripData(this.props.graphParams);   
-  }
+    props.fetchPrecomputedWaitAndTripData(props.graphParams);
+  }, []);  // like componentDidMount, this runs only on first render 
+  
+  const classes = useStyles();
 
-  render() {
-    const {
-      graphParams,
-      routes,
-    } = this.props;
+  const {
+    graphParams,
+    routes,
+  } = props;
 
-    let charts = null;
-    
-    if (routes) {
-      charts = routes.map(route => 
-        route.directions.map(direction => {
-          return <Grid item>Route: {route.id} Direction: {direction.id}<TravelTimeChart route_id={route.id} direction_id={direction.id} /></Grid>;
-        })
-      );
-    }
-      
-    return (
-      <Fragment>
-        <AppBar position="relative">
-          <Toolbar>
-            <SidebarButton />        
-            <div>Muni</div>
-          </Toolbar>
-        </AppBar>
-
-        Date: { graphParams.date} Time: { graphParams.start_time } - { graphParams.end_time }
-        
-        <QuadrantChart/>
-        <Grid container spacing={0}>
-            { charts }
-        </Grid>
-      </Fragment>
+  let charts = null;
+  
+  if (routes) {
+    charts = routes.map(route => 
+      route.directions.map(direction => {
+        return <Grid item key={route.id + direction.id}>Route: {route.id} Direction: {direction.id}<TravelTimeChart route_id={route.id} direction_id={direction.id} /></Grid>;
+      })
     );
   }
+    
+  return (
+    <Fragment>
+      <AppBar position="relative">
+        <Toolbar>
+          <SidebarButton />        
+          <div className={classes.title}>Muni</div>
+          <DateTimePanel/>
+        </Toolbar>
+      </AppBar>
+
+      Date: { graphParams.date} Time: { graphParams.start_time } - { graphParams.end_time }
+      
+      <QuadrantChart/>
+      <Grid container spacing={0}>
+          { charts }
+      </Grid>
+    </Fragment>
+  );
 }
 
 const mapStateToProps = state => ({
