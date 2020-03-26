09401722d2dd1ad9f2b399a37d849d0df395c88f
Neel Mehta <neelmehta96@gmail.com>
2020-03-25 23:03:26 -0400

    be happy, lint

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Link from 'redux-first-router-link';
 
 export default function NotFound() {
-  return <h3>404</h3>;
+  return (
+    <Fragment>
+      <h2>
+        This train went off the rails!{' '}
+        <span role="img" aria-label="train-crash">
+          🚈💥
+        </span>
+      </h2>
+
+      <p>
+        Sorry, but we can&apos;t find what you&apos;re looking for. This link
+        might be malformed.
+      </p>
+
+      <p>
+        Try going <Link to="/">back to the main page</Link>.
+      </p>
+    </Fragment>
+  );
 }
