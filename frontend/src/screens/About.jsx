4af50ec386471c03b5a30fc0dd3aac5e88e71069
akgupta89 <40367955+akgupta89@users.noreply.github.com>
2020-04-29 22:20:26 -0700

    Update About.jsx
    Lets not leave out all the faceless contributors :)

--- frontend/src/screens/About.jsx
+++ frontend/src/screens/About.jsx
@@ -36,7 +36,7 @@ export default function About() {
       </p>
       <p>
         OpenTransit is an open-source project developed by volunteers at Code
-        for PDX and Code for San Francisco.
+        for PDX, Code for San Francisco, and volunteers like you from all around the world.
       </p>
     </div>
   );
